c2a48800c260e94dcfcf84f73c1f6e0894e9baf0
Naturewealth <adebisibaliqeez@gmail.com>
2023-04-21 21:27:13 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print all possible combination of single digit numbers
+ * main - print all possible combinations of single digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -13,11 +13,10 @@ int main(void)
 		putchar(n);
 		if (n != 57)
 		{
-
 			putchar(',');
 			putchar(' ');
 		}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 	}
 }
