ea3a781908a07d355b79992865b9aa36435b37e2
Naturewealth <adebisibaliqeez@gmail.com>
2023-05-05 10:53:05 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,14 @@
  */
 void reverse_array(int *a, int n)
 {
-        int i;
-        int t;
+	int i;
+	int j;
 
-        for (i = 0; i < n--; i++)
-        {
-                t = a[i];
-                a[i] = a[n];
-                a[n] = t;
-        }
+	for (i = 0; i < n; i++)
+	{
+		n--;
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+	}
 }
