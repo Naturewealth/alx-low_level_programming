589d6dc9c599b424bb84234d0c33dc074afe50b4
Naturewealth <adebisibaliqeez@gmail.com>
2023-05-05 17:58:55 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: input string
+ *
+ * Return: the pointer to dest
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= &&n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
